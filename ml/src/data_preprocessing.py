d88e74a7505aea52b5bb245b1e6ab18770d04ee6
di <@Diana-dil>
2025-11-15 18:58:08 +0300

    [feature]: загрузила данные

--- ml/src/data_preprocessing.py
+++ ml/src/data_preprocessing.py
@@ -1,1 +1,6 @@
 import pandas as pd
+import numpy as np
+
+file_path = 'C:/PyCharmProjects/Git_ML/ml/data/raw/train.csv'  # изменить путь на нужный
+data = pd.read_csv(file_path, nrows = 5000000, index_col=0)
+
