dc10b3e3e5cfb2f6942d4de9582ae4b59c4564d4
thesecondpioneer <thesecondpioneer@gmail.com>
2025-12-13 19:48:01 +0300

    [feature]: Добавил реверс прокси в nginx для запросов на бэкенд

--- web-frontend/src/api/client.ts
+++ web-frontend/src/api/client.ts
@@ -1,10 +1,8 @@
-export const API_URL = import.meta.env.VITE_API_URL || "http://localhost:8000";
-
 export async function predictCSV(file: File) {
   const formData = new FormData();
   formData.append("file", file);
 
-  const res = await fetch(`${API_URL}/predict-csv`, {
+  const res = await fetch('/api/predict-csv', {
     method: "POST",
     body: formData,
   });
